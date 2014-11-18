50d7040f893de71166e1d252695310132251c974
Matias Schertel <mschertel@gmail.com>
2014-11-18 12:52:54 -0200

    servlet code cleaned and adapted to our measurement

--- src/com/sap/iot/sensors/SensorsServlet.java
+++ src/com/sap/iot/sensors/SensorsServlet.java
@@ -4,8 +4,9 @@ package com.sap.iot.sensors;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.naming.InitialContext;
@@ -21,135 +22,49 @@ import javax.sql.DataSource;
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.persistence.Measurement;
 
-import com.google.gson.Gson;
 import com.sap.security.core.server.csi.IXSSEncoder;
 import com.sap.security.core.server.csi.XSSEncoder;
 
-/**
- * 
- * Servlet implementation class SensorsServlet
- */
 public class SensorsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private DataSource ds;
 	private EntityManagerFactory emf;
 
-	/**
-	 * 
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public SensorsServlet() {
 		super();
 	}
 
-	/**
-	 * 
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String action = encodeText(request.getParameter("action"));
 		DataHelper dataHelper = new DataHelper(emf);
-		// Add a sensor value
-		if (action != null && action.equalsIgnoreCase("addsensorvalue")) {
+
+		if (action != null && action.equalsIgnoreCase("addmeasurement")) {
 			Measurement measurement = extractMeasurementData(request);
 			dataHelper.addMeasurement(measurement);
 		}
-		// Provide a JSON output of all sensor values (measurements)
-		if (action != null && action.equalsIgnoreCase("showallmeasurements")) {
-			List<Measurement> sensorMeasurements = dataHelper
-					.getAllSensorReadings();
-			outputJsonForAllMeasurements(response, sensorMeasurements);
-		}
 	}
 
-	/*
-	 * 
-	 * Creates a JSON output of all sensor values (measurements)
-	 * 
-	 * 
-	 * 
-	 * @param response The HTTP-response object
-	 * 
-	 * 
-	 * 
-	 * @param sensorMeasurements The list of sensors values (measurements)
-	 */
-	private void outputJsonForAllMeasurements(HttpServletResponse response,
-			List<Measurement> sensorMeasurements) {
-		Gson gson = new Gson();
-		try {
-			response.getWriter().println("{");
-			for (int i = 0; i < sensorMeasurements.size(); i++) {
-				response.getWriter().println(
-						gson.toJson(sensorMeasurements.get(i)));
-				if (i != sensorMeasurements.size() - 1) {
-					response.getWriter().println(",");
-				}
-			}
-			response.getWriter().println("}");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/*
-	 * 
-	 * Extracts a Measurement object (sensor values) out of the parameters
-	 * 
-	 * provided in the HTTP-request
-	 * 
-	 * 
-	 * 
-	 * @param request The HTTP-request object
-	 * 
-	 * 
-	 * 
-	 * @return The derived Measurement object
-	 */
 	private Measurement extractMeasurementData(HttpServletRequest request) {
 		Measurement measurement = new Measurement();
-		// Get sensorId
-		String sensorIdString = encodeText(request.getParameter("sensorid"));
-		if (sensorIdString != null && sensorIdString.length() > 0) {
-			// measurement.setSensorId(Long.parseLong(sensorIdString));
-		}
-		// Get unit of measured value
-		// measurement.setUnit(encodeText(request.getParameter("unit")));
-		// Get measured value and calculate the value to be stored
-		String sensorValue = encodeText(request.getParameter("sensorvalue"));
-		String sensorValueMultiplier = encodeText(request
-				.getParameter("sensorvaluemultiplier"));
-		String sensorValueCalibration = encodeText(request
-				.getParameter("sensorvaluecalibration"));
-		if (sensorValueCalibration != null
-				&& sensorValueCalibration.length() > 0 && sensorValue != null
-				&& sensorValueMultiplier != null
-				&& sensorValueMultiplier.length() > 0
-				&& sensorValue.length() > 0) {
-			// measurement.setStoredAt(new Timestamp(new Date().getTime()));
-			Double valueDouble = Double.parseDouble(sensorValue);
-			Double multiplierDouble = Double.parseDouble(sensorValueMultiplier);
-			Double valueGap = Double.parseDouble(sensorValueCalibration);
-			Double value = (valueDouble * multiplierDouble) + valueGap;
-			// measurement.setValue(value);
+
+		String username = encodeText(request.getParameter("username"));
+		String device = encodeText(request.getParameter("device"));
+		String metric = encodeText(request.getParameter("metric"));
+		String value = encodeText(request.getParameter("value"));
+
+		if (username != null && username.length() > 0 && device != null
+				&& device.length() > 0 && metric != null && metric.length() > 0
+				&& value != null && value.length() > 0) {
+			measurement.setTimestp(new Timestamp(new Date().getTime()));
+			measurement.setUsername(username);
+			measurement.setDevice(device);
+			measurement.setMetric(metric);
+			measurement.setValue(Long.valueOf(value));
 		}
 		return measurement;
 	}
 
-	/*
-	 * 
-	 * Encodes a text to avoid cross-site-scripting vulnerability
-	 * 
-	 * 
-	 * 
-	 * @param request The text to be encoded
-	 * 
-	 * 
-	 * 
-	 * @return The encoded String
-	 */
 	private String encodeText(String text) {
 		String result = null;
 		IXSSEncoder xssEncoder = XSSEncoder.getInstance();
@@ -161,17 +76,11 @@ public class SensorsServlet extends HttpServlet {
 		return result;
 	}
 
-	/**
-	 * 
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		this.doGet(request, response);
 	}
 
-	/** {@inheritDoc} */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void init() throws ServletException {
